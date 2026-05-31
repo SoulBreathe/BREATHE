729fc370907cc354a3f90cfc0b3a3a5b6f8b5c31
SoulBreathe <richardmartins.dev@gmail.com>
2026-05-31 18:32:13 -0300

    fish: initial config

---
+++ home/fish/config.fish
@@ -0,0 +1,16 @@
+# remove greeting
+set fish_greeting
+
+# vim keybinds no terminal
+fish_vi_key_bindings
+
+# aliases uteis
+alias ls='ls --color=auto'
+alias ll='ls -lah'
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+
+# breathe - troca de atmosfera rapido
+alias breathe='~/breathe/scripts/breathe-switch'
